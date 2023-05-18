15d9ffa5354da219791548d4ea0f64c05a8718b3
FuexFollets <fuex9follets@gmail.com>
2023-05-18 12:53:16 -0400

    Fixed file uploading menu

--- src/uploadmenu.tsx
+++ src/uploadmenu.tsx
@@ -1,30 +1,54 @@
 import { useState } from 'react'
 import { Globals, UploadedSoundFileBits } from "./globals"
+import FileData from "./filedata"
 
 function UploadMenu() {
-    const [fileName, setFileName] = useState("Upload")
+    const [buttonText, setButtonText] = useState("Upload")
+    const [fileDataState, setFileDataState] = useState<Uint8Array | undefined>(undefined)
+    const [fileState, setFileState] = useState<File | undefined>(undefined)
+    const [buttonIsDisplayingUndefined, setButtonIsDisplayingUndefined] = useState(false)
+
     let soundFile: File 
+    let u8array: Uint8Array | undefined
 
     async function handleFile(event: React.ChangeEvent<HTMLInputElement>) {
         if (event.target.files) {
             soundFile = event.target.files[0]
+            setFileState(soundFile)
+        }
+
+        soundFile.arrayBuffer().then((buffer) => {
+            u8array = new Uint8Array(buffer)
+            setFileDataState(u8array)
+        })
+
+        if (buttonIsDisplayingUndefined) {
+            setButtonText("Upload")
+            setButtonIsDisplayingUndefined(false)
         }
     }
 
     async function addSoundFile() {
-        const buffer: ArrayBuffer = await soundFile.arrayBuffer()
-        const u8array: Uint8Array = new Uint8Array(buffer)
+        if (fileState === undefined) {
+            setButtonText("Error, no file specified")
+            setButtonIsDisplayingUndefined(true)
+        }
 
-        Globals.allUint8Arrays.push(new UploadedSoundFileBits(u8array))
+        if (u8array) {
+            Globals.allUint8Arrays.push(new UploadedSoundFileBits(u8array))
+        }
 
-        setFileName(`Successfully uploaded "${soundFile.name}"`)
+        else if (fileState) {
+            setButtonText(`Successfully uploaded "${fileState.name}"`)
+        }
     }
 
     return (
         <div>
             <input type="file" onChange={handleFile} />
             <br />
-            <button onClick={addSoundFile}>{fileName}</button>
+            <button onClick={addSoundFile}>{buttonText}</button>
+            <FileData bytes={fileDataState} />
         </div>
     )
 }
