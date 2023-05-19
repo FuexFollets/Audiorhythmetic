970e8d36dcb7e185e0d50339294ed4aed7dac2e8
FuexFollets <fuex9follets@gmail.com>
2023-05-19 09:33:54 -0400

    Added a utility for value waiting

---
+++ src/util/waitfor.tsx
@@ -0,0 +1,14 @@
+export async function waitFor(conditionalFunction: () => boolean) {
+    return new Promise<void>((resolve) => {
+        if (conditionalFunction()) {
+            resolve();
+        }
+
+        else {
+            setTimeout(async () => {
+                await waitFor(conditionalFunction);
+                resolve();
+            }, 100);
+        }
+    })
+}
