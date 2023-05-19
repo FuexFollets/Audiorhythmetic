2f8856cf32de3b9c0cdcfa053f45a3abd40503a0
FuexFollets <fuex9follets@gmail.com>
2023-05-19 09:34:49 -0400

    Added guaranteed initialized wasm module loader

--- src/wasmmod.tsx
+++ src/wasmmod.tsx
@@ -1,7 +1,20 @@
+// import EventEmitter from "events"
 import Module from "./wasm/module.js"
+import { waitFor } from "./util/waitfor.tsx"
 
-export const wasmModule = Module
+const _wasmModuleRaw = Module
+let _wasmModuleInitialized = false
 
-export function texp() {
-    return 8
+_wasmModuleRaw['onRuntimeInitialized'] = () => {
+    _wasmModuleInitialized = true
+}
+
+export async function wasmModuleRaw() {
+    if (_wasmModuleInitialized) {
+        return _wasmModuleRaw
+    }
+
+    await waitFor(() => _wasmModuleInitialized)
+
+    return _wasmModuleRaw
 }
